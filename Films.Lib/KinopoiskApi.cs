818a30a5e747f00132189f64e8428d19213d1649
Starinin Andrey <starininandrey@gmail.com>
2022-05-15 13:47:53 +0300

    Добавление отправки запроса

---
+++ Films.Lib/KinopoiskApi.cs
@@ -0,0 +1,15 @@
+﻿using System.Net;
+using System.Text.Json;
+
+namespace Films.Lib;
+
+public static class KinopoiskApi
+{
+    public static Films SearchFilms(string name)
+    {
+        var str = $"https://api.kinopoisk.dev/movie?token=ZQQ8GMN-TN54SGK-NB3MKEC-ZKB8V06&search={name}&field=name&isStrict=false";
+        using var webStream = WebRequest.Create(str).GetResponse().GetResponseStream();
+        using var stream = new StreamReader(webStream);
+        return JsonSerializer.Deserialize<Films>(stream.ReadToEnd());
+    }
+}
