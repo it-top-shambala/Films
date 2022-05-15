19040d36a46a3d0e4afc5b3f09480392244a78a5
Starinin Andrey <starininandrey@gmail.com>
2022-05-15 14:20:06 +0300

    Получение списка фильмов

--- Films.App/MainWindow.xaml.cs
+++ Films.App/MainWindow.xaml.cs
@@ -1,18 +1,26 @@
-﻿using System.Windows;
+﻿using System.Collections.ObjectModel;
+using System.Windows;
 using System.Windows.Controls;
+using Films.Lib;
 
 namespace Films.App
 {
     public partial class MainWindow : Window
     {
+        public ObservableCollection<Doc> Films { get; set; }
+        private FilmsInfo _films;
         public MainWindow()
         {
+            Films = new ObservableCollection<Doc>();
             InitializeComponent();
         }
 
         private void ButtonSearch_OnClick(object sender, RoutedEventArgs e)
         {
-            throw new System.NotImplementedException();
+            var search = InputSearch.Text;
+            _films = KinopoiskApi.SearchFilms(search);
+            Films = new ObservableCollection<Doc>(_films.docs);
+            ListFilm.ItemsSource = Films;
         }
 
         private void Selector_OnSelectionChanged(object sender, SelectionChangedEventArgs e)
